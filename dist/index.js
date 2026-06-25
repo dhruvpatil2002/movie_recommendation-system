aff49df2215af129bd1a94c643cbd94c7386f15b
dhruvpatil2002 <dhruvpatil713@gmail.com>
2026-06-25 16:08:26 +0530

    the package.json file change

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,13 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const recommended_routes_1 = require("./routes/recommended.routes");
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: [
+        "http://localhost:3000",
+        "https://movie-recommendation-system-pi-ten.vercel.app",
+    ],
+    credentials: true,
+}));
 app.use(express_1.default.json());
 app.get("/health", (_req, res) => {
     res.json({ status: "ok" });
